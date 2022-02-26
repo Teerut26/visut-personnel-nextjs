2c84d8a90b8f1b03d96753a0c12423c5e64bff83
Teerut26 <teerutsr@gmail.com>
2022-02-27 00:12:32 +0700

    add count

--- pages/index.js
+++ pages/index.js
@@ -15,39 +15,61 @@ export async function getServerSideProps(context) {
         allPerson{
           _id
         }
-        allPage(where:{}){
-          title
-          slug
-        }
+        allPage{
+            title
+            slug
+            position {
+              title
+              person{
+                _id
+              }
+            }
+          }
       }`,
   });
+
+  let nav_lists = data.data.allPage.map((item) => {
+    let counter = 0;
+    item.position.map((item2) => {
+      counter += item2.person.length;
+    });
+
+    return {
+      ...item,
+      person_count: counter,
+    };
+  });
+
   return {
     props: {
-      nav_lists: data.data.allPage,
+      nav_lists,
       personCount: data.data.allPerson.length,
     }, // will be passed to the page component as props
   };
 }
 
 function Home({ nav_lists, personCount }) {
-
   return (
     <>
       <WithNavbar navlists={nav_lists}>
-        <div className="flex justify-center py-5">
+        <div className="flex justify-center gap-3 py-5">
           <div className="w-[10rem] h-[10rem] border-2 rounded-full flex flex-col justify-center items-center bg-blue-400 text-white">
             <div className="text-3xl font-bold">{personCount}</div>
             <div>บุคลากรทั้งหมด</div>
           </div>
+          <div className="w-[10rem] h-[10rem] border-2 rounded-full flex flex-col justify-center items-center bg-blue-400 text-white">
+            <div className="text-3xl font-bold">{nav_lists.length}</div>
+            <div>กลุ่มสาระ</div>
+          </div>
         </div>
-        {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 md:gap-12 m-2 justify-center max-w-6xl mx-auto p-3">
-          <Person />
-          <Person />
-          <Person />
-          <Person />
-          <Person />
-          <Person />
-        </div> */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 max-w-3lg mx-auto px-3 mb-3">
+          {nav_lists.map((item) => (
+            <div className=" p-3 flex flex-col justify-between items-center bg-blue-400 rounded-xl text-white">
+              <div className="text-lg">{item.title}</div>
+              <div className="text-3xl">{item.person_count} คน</div>
+            </div>
+          ))}
+        </div>
       </WithNavbar>
     </>
   );
